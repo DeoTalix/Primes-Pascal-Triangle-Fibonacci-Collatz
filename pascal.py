b90249b2a8322d443042394798ec944f330247bd
ckounter <45055549+GladunovDenis@users.noreply.github.com>
2021-09-30 18:08:41 +0000

    Added Pascal Triangle but Collatz(a,b) instead of add(a,b).

--- pascal.py
+++ pascal.py
@@ -1,4 +1,4 @@
-def pascal_triangle(X, Y):
+def pascal_triangle(X, Y, oper=lambda a,b: a+b, half=False):
     """
     X: iterable, Y: iterable
     """
@@ -8,15 +8,16 @@ def pascal_triangle(X, Y):
     for i in range(1, len(X)):
         matrix[0][i] = X[i-1]
 
-    #for j in range(1, len(Y)):
-    #    matrix[j][0] = Y[j-1] 
+    if half == False:
+        for j in range(1, len(Y)):
+            matrix[j][0] = Y[j-1] 
 
     for i in range(1, len(Y)):
-        for j in range(i, len(X)):
-            if i == j:
-                matrix[i][j] = matrix[i-1][j] * 2
+        for j in range(i if half else 1, len(X)):
+            if half and i == j:
+                matrix[i][j] = oper(matrix[i-1][j], matrix[i-1][j])
             else:
-                matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+                matrix[i][j] = oper(matrix[i-1][j], matrix[i][j-1])
 
     return matrix
 
@@ -24,20 +25,20 @@ def print_matrix(M, callback=(lambda cell: cell), format=(lambda cell: cell), se
     for row in M:
         for v in row:
             v_ = callback(v)
-            print(format(v), end='')
+            print(format(v_), end='')
         print()
 
 def main():
     from primes.source import Primes, is_prime
     from primes.utility import save_primes, load_primes
 
-    X = [1] * 65#range(1,60)
-    Y = [1] * 65#range(1,60)
-    Pt = pascal_triangle(X, Y)
+    #X = [1] * 65#range(1,60)
+    #Y = [1] * 65#range(1,60)
+    #Pt_ones = pascal_triangle(X, Y, half=True)
 
     if 0:
         primes = load_primes()
-        max_val = Pt[-1][-1]
+        max_val = Pt_ones[-1][-1]
         primes_iter = Primes(max_val, primes)
         
         try:
@@ -63,13 +64,25 @@ def main():
     odd_even       = (lambda v: ('o' if v % 10 in [1, 3, 7, 9] else '.') if v % 2 else ' ')
     prime_mod_nine = (lambda v: '*' if v % 9 in [1, 3, 7, 9] else ' ')
     
+    # Dotted Serpinski Pattern
+    # print_matrix(Pt_ones, format = (lambda v: " ." if v%2 else '  '), sep='')
+
+    # X = [1] * 65#range(1,60)
+    # Y = [1] * 65#range(1,60)
+    # Pt_ones = pascal_triangle(X, Y, half= False)
+
+    # 3 6 9 pattern
+    # print_matrix(Pt_ones, callback=lambda v: v % 9 or 9 if v % 9 in [3,6,0] else ' ', format=lambda v: f"{v} ")
+
+    X = range(1, 60)
+    Y = range(1, 60)
+    
+    Pt_collatz = pascal_triangle(X, Y, oper=lambda a,b: (3*(a+b)+1) if (a+b)%2 else (a+b)//2,)
     print_matrix(
-        Pt, 
-        #callback = prime_dots,
-        #callback = odd_even,
-        format = (lambda v: " ." if v%2 else '  '),
-        sep=''
-    )
+        Pt_collatz, 
+        callback=lambda v: v%9 or 9 if v else 0, 
+        format=lambda v: f". " if v!=7 else '  ',
+    ) # A fractal ?
 
 if __name__ == "__main__":
     main()
